d727ccfb08fa3ac5dc88f1284b25946a55b9110e
Bruno Bressi <bruno.bressi@telekom.de>
2024-10-01 13:41:10 +0200

    feat: added Makefile
    added makefile to bundle chart

---
+++ hack/chart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+helm dependency update
+tarball=$(ls charts/kube-prometheus-stack-*.tgz)
+echo "Extracting ${tarball}"
+tar -xzf ${tarball} -C charts/
+
+echo "Deleting CRD from kube-prometheus-stack"
+rm -rf charts/kube-prometheus-stack/charts/crd/
+
+echo "Packaging new chart"
+helm package charts/kube-prometheus-stack -d charts
+
+echo "Packaging caas-project-monitoring"
+helm package .
